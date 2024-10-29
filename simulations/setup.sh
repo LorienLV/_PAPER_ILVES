b6874e77b4474fc4c9d119fade174b915ca632b4
Lorien <lorienlopezv@gmail.com>
2024-10-29 16:23:37 +0100

    Move setup.sh

--- simulations/setup.sh
+++ simulations/setup.sh
@@ -3,4 +3,4 @@
 export ROOT="$(dirname $(realpath $0))"
 
 export GMX_MPI="PATH_TO_GROMACS_DOUBLE_INSTALLATION/gmx_mpi_d"
-export GMX_MPI_MOD="PATH_TO_GROMACS_WITH_ILVES_DOUBLE_INSTALLATION/gmx_mpi_d"
+export GMX_MPI_MOD="/home/lorien/Repos/ilves_paper_bin/build_double_gcc_mpi/bin/gmx_mpi_d"
