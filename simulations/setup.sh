e0e395bcd1d591bde4a3d2100e64bd16a2fbb020
Lorien <lorienlopezv@gmail.com>
2024-10-30 16:14:29 +0100

    Generic path to gromacs binary

--- simulations/setup.sh
+++ simulations/setup.sh
@@ -4,4 +4,4 @@
 export SIMULATIONS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export GMX_MPI="PATH_TO_GROMACS_DOUBLE_INSTALLATION/gmx_mpi_d"
-export GMX_MPI_MOD="/home/lorien/Repos/ilves_paper_bin/build_double_gcc_mpi/bin/gmx_mpi_d"
+export GMX_MPI_MOD="PATH_TO_GROMACS_WITH_ILVES_DOUBLE_INSTALLATION/gmx_mpi_d"
